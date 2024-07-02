3dffc16a655f4cd6adaba599aeb15b43ca4716a4
ensklc93 <143909013+ensklc93@users.noreply.github.com>
2024-07-02 14:47:30 +0200

    Added the code for passport middleware
    Integrated the Passport strategies as middleware into each of the existing API

--- index.js
+++ index.js
@@ -15,7 +15,13 @@ mongoose.connect("mongodb://localhost:27017/cfDB", {
   useUnifiedTopology: true,
 })
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
+
+let auth = require('./auth')(app);
+
+const passport = require('passport');
+require('./passport.js');
 
 app.use(express.static("public"))
 app.use(morgan("common"))
@@ -24,7 +30,7 @@ app.get("/", (req, res) => {
   res.send("Welcome to my Movie Archive!")
 })
 
-app.get("/movies", async (req, res) => {
+app.get("/movies", passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Movies.find()
     .then(movies => {
       res.status(201).json(movies)
@@ -35,7 +41,7 @@ app.get("/movies", async (req, res) => {
     })
 })
 
-app.get("/movies/:Title", async (req, res) => {
+app.get("/movies/:Title", passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Movies.findOne({ Title: req.params.Title })
     .then(movie => {
       res.json(movie)
@@ -46,7 +52,7 @@ app.get("/movies/:Title", async (req, res) => {
     })
 })
 
-app.get("/movies/genre/:name", async (req, res) => {
+app.get("/movies/genre/:name", passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Movies.findOne({ "Genre.Name": req.params.name })
     .then(movie => {
       res.status(201).json(movie.Genre)
@@ -57,7 +63,7 @@ app.get("/movies/genre/:name", async (req, res) => {
     })
 })
 
-app.get("/movies/directors/:name", async (req, res) => {
+app.get("/movies/directors/:name", passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Movies.findOne({ "Director.Name": req.params.name })
     .then(movie => {
       res.status(201).json(movie.Director)
@@ -72,7 +78,7 @@ app.post("/users", async (req, res) => {
   await Users.findOne({ Username: req.body.Username })
     .then(user => {
       if (user) {
-        return res.status(400).send(req.body.Username + "already exists")
+        return res.status(400).send(req.body.Username + " already exists")
       } else {
         Users.create({
           Username: req.body.Username,
@@ -95,7 +101,7 @@ app.post("/users", async (req, res) => {
     })
 })
 
-app.get("/users", async (req, res) => {
+app.get("/users", passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Users.find()
     .then(users => {
       res.status(201).json(users)
@@ -106,7 +112,7 @@ app.get("/users", async (req, res) => {
     })
 })
 
-app.get("/users/:Username", async (req, res) => {
+app.get("/users/:Username", passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Users.findOne({ Username: req.params.Username })
     .then(user => {
       res.json(user)
@@ -117,29 +123,32 @@ app.get("/users/:Username", async (req, res) => {
     })
 })
 
-app.put("/users/:Username", async (req, res) => {
-  await Users.findOneAndUpdate(
-    { Username: req.params.Username },
-    {
-      $set: {
-        Username: req.body.Username,
-        Password: req.body.Password,
-        Email: req.body.Email,
-        Birthday: req.body.Birthday,
-      },
-    },
-    { new: true }
-  ) // This line makes sure that the updated document is returned
-    .then(updatedUser => {
-      res.json(updatedUser)
-    })
-    .catch(err => {
-      console.error(err)
-      res.status(500).send("Error: " + err)
-    })
-})
+app.put('/users/:Username', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  
+  if(req.user.Username !== req.params.Username){
+      return res.status(400).send('Permission denied');
+  }
+ 
+  await Users.findOneAndUpdate({ Username: req.params.Username }, {
+      $set:
+      {
+          Username: req.body.Username,
+          Password: req.body.Password,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday
+      }
+  },
+      { new: true }) // This line makes sure that the updated document is returned
+      .then((updatedUser) => {
+          res.json(updatedUser);
+      })
+      .catch((err) => {
+          console.log(err);
+          res.status(500).send('Error: ' + err);
+      })
+});
 
-app.post("/users/:Username/movies/:MovieID", async (req, res) => {
+app.post("/users/:Username/movies/:MovieID", passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Users.findOneAndUpdate(
     { Username: req.params.Username },
     {
@@ -156,7 +165,7 @@ app.post("/users/:Username/movies/:MovieID", async (req, res) => {
     })
 })
 
-app.delete("/users/:Username/movies/:MovieID", async (req, res) => {
+app.delete("/users/:Username/movies/:MovieID", passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Users.findOneAndUpdate(
     { Username: req.params.Username },
     {
@@ -173,7 +182,7 @@ app.delete("/users/:Username/movies/:MovieID", async (req, res) => {
     })
 })
 
-app.delete('/users/:Username', async (req, res) => {
+app.delete('/users/:Username', passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Users.findOneAndUpdate({ Username: req.params.Username })
     .then((user) => {
       if (!user) {
